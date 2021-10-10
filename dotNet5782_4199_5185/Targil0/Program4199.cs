b5a45927e1f2feb51cbd3a386a2c390ac7fcc2dc
Elyashiv Tzur <etzur@g.jct.ac.il>
2021-10-10 16:28:46 +0300

    Initial code - Welcome functions

---
+++ dotNet5782_4199_5185/Targil0/Program4199.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4199();
+            Welcome5185();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5185();
+        private static void Welcome4199()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcome to my first console application", name);
+        }
+    }
+}
