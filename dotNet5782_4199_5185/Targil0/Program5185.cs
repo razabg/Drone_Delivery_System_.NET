ce2023a7246832e61e37606facec9e02d7dd7a69
razabg <razabg@gmail.com>
2021-10-10 16:54:44 +0300

    i added new func

---
+++ dotNet5782_4199_5185/Targil0/Program5185.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welcome5185()
+        {
+            Console.WriteLine("hi im here");
+        }
+
+    }
+
+}
