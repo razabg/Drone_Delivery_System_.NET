3e613469a956cf0acf97bff994e5dfdd03693043
razabg <razabg@gmail.com>
2021-10-26 21:13:04 +0300

    update

--- dotNet5782_4199_5185/DAL/Customer.cs
+++ dotNet5782_4199_5185/DAL/Customer.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 
-namespace DAL
+namespace IDAL
 {
-    public class Class1
+    namespace DO
     {
+        public struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string phone { get; set; }
+            public double Longitude { get; set; } 
+            public double latitude { get; set; }
+
+
+        }
+
     }
 }
--- dotNet5782_4199_5185/DAL/Parcel.cs
+++ dotNet5782_4199_5185/DAL/Parcel.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace IDAL
 {
-    class Class1
+    namespace DO
     {
+        struct Parcel
+        {
+            public int Id { get; set; }
+            public int SenderId { get; set; }
+            public int TargetId { get; set; }
+            public float Weight { get; set; }
+            public int Priority { get; set; }
+            public int DroneId { get; set; }
+            public DateTime Scheduled { get; set; }
+            public DateTime PickedUp { get; set; }
+            public DateTime Delivered { get; set; }
+            public DateTime Requested { get; set; }
+
+
+
+
+
+
+
+        }
+
+
     }
 }
