29883c127af3e6fc2614845c7bfd8d891b981dd0
Elyashiv Tzur <etzur@g.jct.ac.il>
2021-10-27 01:06:08 +0300

    some new things...

---
+++ dotNet5782_4199_5185/DAL/DalObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace IDAL.DO
+{
+    namespace DalObject
+    {
+
+        public class DataSource
+        {
+            internal static List<Drone> DronesList = new List<Drone>();
+            internal static List<Station> StationsList = new List<Station>();
+            internal static List<Customer> CustomersList = new List<Customer>();
+            internal static List<Parcel> ParcelsList = new List<Parcel>();
+
+            static Random rand = new Random();
+            const int MinRange = 111111111;
+            const int MaxRange = 999999999;
+            static void Initialize()
+            {
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    StationsList.Add(new Station()
+                    {
+                        Id = rand.Next(MinRange, MaxRange),
+                        Name = rand.Next(MinRange / 3, MaxRange / 3),
+                        ChargeSlots= rand.Next(1, 100),
+                        Longitude=rand.Next(111111111,999999999)
+                    }) 
+                }
+                
+
+            }
+        }
+    }
+}
