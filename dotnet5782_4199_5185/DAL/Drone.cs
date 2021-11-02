484b3aaeff780e1df0cffaaa65a0da0186e0cffe
razabg <razabg@gmail.com>
2021-11-02 21:42:05 +0200

    change

--- dotnet5782_4199_5185/DAL/Drone.cs
+++ dotnet5782_4199_5185/DAL/Drone.cs
@@ -14,16 +14,15 @@ namespace IDAL
             public string Model { get; set; }
             public string MaxWeight { get; set; }
             public string Status { get; set; } 
-            public double Battary { get; set; }
-
+            
             public override string ToString()
             {
                 string printDroneInfo = "";
                 printDroneInfo += $" the Id is {Id},\n";
                 printDroneInfo += $"the model is{Model},\n";
                 printDroneInfo += $"the MaxWeight is{MaxWeight},\n";
-                printDroneInfo += $"the Status is {Status},\n";
-                printDroneInfo += $"Battary left {Battary},\n";
+                //printDroneInfo += $"the Status is {Status},\n";
+               // printDroneInfo += $"Battary left {Battary},\n";
                 return printDroneInfo;
             }
         }
