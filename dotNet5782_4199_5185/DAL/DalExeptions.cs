a93f0b4a81141aeae8d0d888763a8daf678a55c7
razabg <razabg@gmail.com>
2021-12-19 11:13:01 +0200

    change

--- dotNet5782_4199_5185/DAL/DalExeptions.cs
+++ dotNet5782_4199_5185/DAL/DalExeptions.cs
@@ -16,7 +16,7 @@ namespace DalObject
         public int ID;
         public AlreadyExistException() : base() { }
         public AlreadyExistException(string msg) { Console.WriteLine(msg); }
-        //public NotExistException(int id, string message, Exception innerException) : base(message, innerException) => ID = ((IDAL.DO.NotExistException)innerException).ID;
+        
         public override string ToString() => base.ToString() + $",The id does not exist";
 
     }
