fe353f5d253035d1760d01a9895712f824113472
razabg <razabg@gmail.com>
2022-01-03 22:27:27 +0200

    TE

--- dotnet5782_4199_5185/PL/DroneList.xaml.cs
+++ dotnet5782_4199_5185/PL/DroneList.xaml.cs
@@ -31,7 +31,7 @@ namespace PL
             BLAccess = BlFactory.GetBl();
             DroneListView.DataContext = BLAccess.GetDroneToLists();
             dronestatus.ItemsSource = Enum.GetValues(typeof(statusEnum.DroneStatus));
-            MaxWeight.ItemsSource = Enum.GetValues(typeof(statusEnum.Weight));
+            MaxWeight.ItemsSource = Enum.GetValues(typeof(statusEnum.TopWeight));
 
             collection = new ObservableCollection<DroneToList>(BLAccess.GetDroneToLists());
             DroneListView.ItemsSource = collection;
@@ -99,6 +99,11 @@ namespace PL
         private void close_window_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void updateList_Click(object sender, RoutedEventArgs e)
+        {
+            DroneListView.DataContext = BLAccess.GetDroneToLists();
         }
     }
 }
